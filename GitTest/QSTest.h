263ea5e0823900e84f4764a8d4fe2b5b50ff6532
Soren Christian Bojer <bojer@sorens-mbp.lan>
2014-06-10 14:06:50 +0200

    add a test class

---
+++ GitTest/QSTest.h
@@ -0,0 +1,13 @@
+//
+//  QSTest.h
+//  GitTest
+//
+//  Created by Soren Christian Bojer on 10/06/14.
+//  Copyright (c) 2014 Soren Christian Bojer. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface QSTest : NSObject
+
+@end
---
+++ GitTest/QSTest.m
@@ -0,0 +1,13 @@
+//
+//  QSTest.m
+//  GitTest
+//
+//  Created by Soren Christian Bojer on 10/06/14.
+//  Copyright (c) 2014 Soren Christian Bojer. All rights reserved.
+//
+
+#import "QSTest.h"
+
+@implementation QSTest
+
+@end
